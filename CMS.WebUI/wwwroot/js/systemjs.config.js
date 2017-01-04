e7f44a4efe86a088577d0b0be03d8471dec3ade0
stefa08 <stefan.pascaru08@gmail.com>
2017-01-04 21:40:54 +0200

    fixed config angular2

--- CMS.WebUI/wwwroot/js/systemjs.config.js
+++ CMS.WebUI/wwwroot/js/systemjs.config.js
@@ -3,43 +3,39 @@
  * Adjust as necessary for your application needs.
  */
 (function (global) {
-    // map tells the System loader where to look for things
-    var map = {
-        // our app is within the app folder
-        'app': 'app',
-        '@angular': 'lib/@angular',
-        'rxjs': 'lib/rxjs'
-    };
-    // packages tells the System loader how to load when no filename and/or no extension
-    var packages = {
-        'app': {
-            main: './main.js',
-            defaultExtension: 'js'
+    System.config({
+        paths: {
+            // paths serve as alias
+            'npm:': '../lib/'
         },
-        'rxjs': {
-            defaultExtension: 'js'
+        // map tells the System loader where to look for things
+        map: {
+            // our app is within the app folder
+            app: '../app',
+
+            // angular bundles
+            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+            '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+            // other libraries
+            'rxjs': 'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+        },
+        // packages tells the System loader how to load when no filename and/or no extension
+        packages: {
+            app: {
+                main: './main.js',
+                defaultExtension: 'js'
+            },
+            rxjs: {
+                defaultExtension: 'js'
+            }
         }
-    };
-    var ngPackegeNames = [
-        'common',
-        'compiler',
-        'core',
-        'http',
-        'platform-browser',
-        'platform-browser-dynamic',
-        'router',
-        'router-deprecated',
-        'upgrade'
-    ];
-    ngPackegeNames.forEach(function (pkgName) {
-        packages['@angular/' + pkgName] = {
-            main: pkgName + '.umd.js',
-            defaultExtension: 'js'
-        };
     });
-    var config = {
-        map: map,
-        packages: packages
-    };
-    System.config(config);
 })(this);
